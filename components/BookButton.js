ae4df2b9b2730786a8845e158d7084abd9c64488
ddnamn <notimportantfile931900@gmail.com>
2023-05-31 20:05:58 +0700

    little adjustment Wishlist screen.

---
+++ components/BookButton.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+export default function ViewAllButton({ children }) {
+  return (
+    <Pressable onPress={() => {}}>
+      <View>
+        <Text style={styles.container}>{children}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#595CEB",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
--- screens/WishlistScreen.js
+++ screens/WishlistScreen.js
@@ -1,11 +1,8 @@
-import { StyleSheet, View, Text, Button, Image } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
-import {
-  Ionicons,
-  Feather,
-  MaterialIcons,
-  FontAwesome5,
-} from "@expo/vector-icons";
+import { MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+
+import BookButton from "../components/BookButton";
 
 export default function WishlistScreen() {
   return (
@@ -13,9 +10,7 @@ export default function WishlistScreen() {
       <View style={styles.wishlistContainer}>
         {/* topbar */}
         <View style={styles.topContainer}>
-          <Ionicons name="ios-arrow-back" size={24} color="black" />
           <Text style={styles.wishlistTitle}>Wishlist</Text>
-          <Feather name="search" size={24} color="black" />
         </View>
 
         <View style={styles.listContainer}>
@@ -33,19 +28,27 @@ export default function WishlistScreen() {
               <Text style={styles.cardCategory}>
                 {"HOTEL" || state.category}
               </Text>
-              <Text style={styles.cardTitle}>{"OmahManis" || state.title}</Text>
-              <View style={styles.locationContainer}>
-                <MaterialIcons name="location-pin" size={24} color="blue" />
-                <Text style={styles.locationTitle}>
-                  {"Bantul,Yogyakarta" || state.location}
+              <View style={styles.titleLocationContainer}>
+                <Text style={styles.cardTitle}>
+                  {"Rumah Mertua" || state.title}
                 </Text>
+                <View style={styles.locationContainer}>
+                  <MaterialIcons
+                    name="location-pin"
+                    size={15}
+                    color="#595CEB"
+                  />
+                  <Text style={styles.locationTitle}>
+                    {"Bantul, Yogyakarta" || state.location}
+                  </Text>
+                </View>
               </View>
               <Text style={styles.price}>{"IDR 215.000" || state.price}</Text>
             </View>
             {/* button */}
             <View style={styles.actionContainer}>
-              <FontAwesome5 name="trash" size={20} color="black" />
-              <Button title="Book" style={styles.button} />
+              <FontAwesome5 name="trash" size={15} color="black" />
+              <BookButton>BOOK</BookButton>
             </View>
           </View>
         </View>
@@ -70,53 +73,58 @@ const styles = StyleSheet.create({
     height: "6%",
     paddingHorizontal: 15,
     marginBottom: 5,
+    marginTop: 1,
   },
   wishlistTitle: {
     flex: 1,
     fontWeight: "bold",
     fontSize: 20,
     paddingHorizontal: 20,
+    textAlign: "center",
+    letterSpacing: 4,
   },
   // listCard
   listContainer: {
+    width: "100%",
     backgroundColor: "#D2D2D2",
     height: "100%",
-
     alignItems: "center",
   },
   //Card
   cardContainer: {
     backgroundColor: "white",
     flexDirection: "row",
-    width: "96%",
-    margin: 10,
+    height: "18%",
+    width: "100%",
+    marginTop: 10,
     borderRadius: 15,
-    padding: 2,
+    padding: 5,
     justifyContent: "space-between",
   },
   imageStyle: {
-    flex: 1, //width: 70,
-    height: 100,
+    width: "26%",
+    height: "100%",
     borderRadius: 10,
-    margin: 4,
   },
   cardInfoContainer: {
-    borderLeftWidth: 1,
-    borderLeftColor: "#424242",
-    padding: 5,
+    flex: 1,
+    padding: 10,
     alignItems: "flex-start",
-    justifyContent: "space-evenly",
+    justifyContent: "space-between",
   },
   cardCategory: {
-    color: "#9D9D9D",
+    color: "#595CEB",
     fontWeight: "bold",
     fontSize: 12,
-    paddingLeft: 5,
+    letterSpacing: 4,
+  },
+  titleLocationContainer: {
+    flex: 1,
+    paddingTop: 6,
   },
   cardTitle: {
     fontWeight: "bold",
-    fontSize: 20,
-    paddingLeft: 5,
+    fontSize: 15,
   },
   locationContainer: {
     flexDirection: "row",
@@ -124,10 +132,12 @@ const styles = StyleSheet.create({
 
   locationTitle: {
     flexDirection: "row",
+    fontSize: 12,
+    paddingLeft: 5,
   },
 
   price: {
-    color: "blue",
+    color: "#595CEB",
     fontWeight: "bold",
     paddingLeft: 5,
   },
@@ -136,6 +146,10 @@ const styles = StyleSheet.create({
   actionContainer: {
     padding: 10,
     alignItems: "flex-end",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
+  },
+
+  contentContainer: {
+    paddingVertical: 20,
   },
 });
